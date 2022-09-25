44c19d8994ce7be98d7ab1e1695e5440f82888bf
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-09-25 07:52:06 +0530

    Create 1) Basic2D.cpp

---
+++ Simple C++ Programs/Loops/Nested loop/1) Basic2D.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout<<i<<j<<endl;
+        }
+    }
+    return 0;
+}
