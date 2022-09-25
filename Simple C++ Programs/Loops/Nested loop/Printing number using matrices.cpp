ed219aa61f0ee06357045ac719e24a4065169270
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-09-25 08:18:03 +0530

    Create Printing number using matrices.cpp

---
+++ Simple C++ Programs/Loops/Nested loop/Printing number using matrices.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,count=1;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
