c0fe53d3f0fa90cac6d0f5d018192dda38d4845b
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-09-25 08:36:21 +0530

    Create Basic 3.cpp

---
+++ Simple C++ Programs/Loops/Nested loop/Printing '*'/Basic 3.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(i+j>5-1)
+            {
+                cout<<"*";
+            }
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
