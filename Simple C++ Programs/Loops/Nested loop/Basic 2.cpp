1638035588e87694317a5e7da781562cad059789
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-09-25 07:59:56 +0530

    Create Basic 2.cpp

---
+++ Simple C++ Programs/Loops/Nested loop/Basic 2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            cout<<"("<<i<<','<<j<<')'<<endl;
+        }
+    }
+    return 0;
+}
