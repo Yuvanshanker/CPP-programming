e9046663be8b24811130872109bd197cfc3d57e5
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-09-29 09:02:47 +0530

    Create Printing right angle triangle using '*'.cpp

---
+++ Simple C++ Programs/Loops/Nested loop/Printing right angle triangle using '*'.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*inverted triangle*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5-i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
