95edc899540c71ba6f9b593b3c1fbbbaca2e6df2
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-09-29 09:10:40 +0530

    Update and rename Printing right angle triangle using '*'.cpp to 1.Printing right angle triangle using '*'.cpp

---
+++ Simple C++ Programs/Loops/Nested loop/1.Printing right angle triangle using '*'.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*inverted triangle*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5-i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*left angled triangle*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i+j>5)
+            {
+                cout<<"* ";
+            }
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/*inverted triangle*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i<j)
+            {
+                cout<<"* ";
+            }
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
