ec8f4684071d566ba13827f4f8157444a4ba15ec
Yuvanshanker <65071471+Yuvanshanker@users.noreply.github.com>
2022-10-11 20:41:26 +0530

    Create 1. Reference example code.cpp

---
+++ Simple C++ Programs/Pointers/Reference/1. Reference example code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x=10;
+    int &y=x;
+    
+    cout<<x<<endl;
+    y++;
+    x++;
+    cout<<x<<endl;
+    cout<<&x<<" "<<&y;
+    
+    return 0;
+}
